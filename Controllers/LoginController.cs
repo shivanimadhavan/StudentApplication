e3c7fc66060e6814cf86db8109a33206f0254c7e
Shivani.M <shivanimadhavan999@gmail.com>
2021-10-25 22:08:39 +0530

    StdApp

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using StudentApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+        shivaniEntities db = new shivaniEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(user objchk)
+        {
+            if (ModelState.IsValid)
+            {
+                using (shivaniEntities db = new shivaniEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objchk.username) && a.password.Equals(objchk.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "The UserName or Password Incorrect");
+
+                    }
+                }
+            }
+            return View(objchk);
+        }
+           
+         public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+        }
+    }
